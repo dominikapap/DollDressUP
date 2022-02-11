7b799148f14d5f40be6a905a0a378aa239a4c949
dominikapap <dominikasz88@interia.pl>
2022-02-11 12:16:56 +0100

    added README and .gitignore, added buttons for changing option and a main function for them in js file

--- myscript.js
+++ myscript.js
@@ -1,4 +1,40 @@
 
+
+
+function changeItem(idName, imgPath, direction){
+    /*checking which image file is currently displayed */
+    const element = document.getElementById(`${idName}`)
+    const bgImage= getComputedStyle(element).backgroundImage
+
+    if (direction == 'next'){
+        var imgNum=parseInt(bgImage[bgImage.length-7])+1
+        /*changing image source url */
+        if (imgNum<7){
+            var urlNum=`${imgPath}`+imgNum+'.png'
+            element.setAttribute("style", `background-image:url(${urlNum})`);
+        }
+        else {
+            imgNum = 1
+            var urlNum=`${imgPath}`+imgNum+'.png'
+            element.setAttribute("style", `background-image:url(${urlNum})`);
+        }
+    }else if(direction == 'prev'){
+        var imgNum=parseInt(bgImage[bgImage.length-7])-1
+        if (imgNum>1){
+            const urlNum=`${imgPath}`+imgNum+'.png'
+            element.setAttribute("style", `background-image:url(${urlNum})`);
+        }
+        else {
+            imgNum = 6
+            var urlNum=`${imgPath}`+imgNum+'.png'
+            element.setAttribute("style", `background-image:url(${urlNum})`);
+        }
+    }
+    
+}
+
+/*for later use
+
 function changeHair(i){
     let imageurl='hair/fronthair'+i+'.png'
     let imageurl2='hair/backhair'+i+'.png'
@@ -10,19 +46,4 @@ function changeShoes(i){
     let imgurlshoes='shoes/shoes'+i+'.png'
     document.getElementById('shoes').setAttribute("style", `background-image:url(${imgurlshoes})`);
 }
-
-function nextBody(){
-    const element = document.getElementById('body')
-    const style = getComputedStyle(element)
-    const bgimage= style.backgroundImage
-    var imgnum=parseInt(bgimage[bgimage.length-7])+1
-    if (imgnum<6){
-        const urlbody='body/body'+imgnum+'.png'
-        document.getElementById('body').setAttribute("style", `background-image:url(${urlbody})`);
-    }
-    else {
-        imgnum = 1
-        const urlbody='body/body'+imgnum+'.png'
-        document.getElementById('body').setAttribute("style", `background-image:url(${urlbody})`);
-    }
-}
+*/
