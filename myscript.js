248f2f7433cad22c1d3ca805c4758ce5d190dd50
dominikapap <dominikasz88@interia.pl>
2022-02-15 10:14:55 +0100

    added button to randomize options, moved fronthair and backhair images to their own folders

--- myscript.js
+++ myscript.js
@@ -7,20 +7,39 @@ function changeItem(idName, imgPath, direction){
     let offset = {"next":1, "prev":-1};
     let currImgNum= parseInt(bgImage[bgImage.length-7]); /*gets img number from url*/
     let newImgNum = currImgNum + offset[direction];/*change displayed img */
-    let maxImgsCount = 6;
+    let maxImgNum = 6;
         
     if (newImgNum < 1){
-        newImgNum = maxImgsCount;
+        newImgNum = maxImgNum;
     };
 
-    if (newImgNum > maxImgsCount){
+    if (newImgNum > maxImgNum){
         newImgNum = 1;
     };
         
-    let urlNum=`${imgPath}`+newImgNum+'.png';
-    element.setAttribute("style", `background-image:url(${urlNum})`);
+    let url=`${imgPath}`+newImgNum+'.png';
+    element.setAttribute("style", `background-image:url(${url})`);
 };
 
+
+
+function randomize(){
+    var elList=[ "fronthair", "face", "body", "shoes"]
+
+    for(let i=0; i< elList.length; i++){
+        let element = document.getElementById(elList[i]);
+        let randNum =Math.floor(Math.random() * 5) +1;
+        let url=`${elList[i]}/${elList[i]}`+randNum+'.png';
+        element.setAttribute("style", `background-image:url(${url})`);
+
+        /*to make sure backhair matches fronthair */
+        if (elList[i] == "fronthair"){
+            let element = document.getElementById("backhair");
+            let url="backhair/backhair"+randNum+'.png';
+            element.setAttribute("style", `background-image:url(${url})`);
+        }
+    }
+}
 
 /*for later use
 
