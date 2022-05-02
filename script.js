1910be8be2482cbe2c3252c490b41831263a2a19
dominikapap <dominikasz88@interia.pl>
2022-05-02 13:57:35 +0200

    formatted script differently

--- script.js
+++ script.js
@@ -5,17 +5,24 @@ let shoesBtns = document.querySelectorAll(".shoes-btn");
 let btnRandom = document.getElementById("btn-random");
 let btnDownload = document.getElementById("btn-download");
 let controlBtns = document.querySelectorAll(".btn-control");
-let dollParts = ['fronthair','fronthair', 'face','face', 'body', 'body', 'shoes', 'shoes','backhair'];
+let dollParts = [
+  "fronthair",
+  "fronthair",
+  "face",
+  "face",
+  "body",
+  "body",
+  "shoes",
+  "shoes",
+  "backhair",
+];
 
-
-//function for changing doll parts with arrow butons 
+//function for changing doll parts with arrow butons
 function changeItem(idName, direction) {
   const element = document.getElementById(`${idName}`);
   const bgImage = getComputedStyle(element).backgroundImage;
-  const currImgNum = parseInt(
-    bgImage[bgImage.length - 7]
-  ); //gets img number from url
-  let newImgNum = currImgNum + direction; //change img source to next or previous 
+  const currImgNum = parseInt(bgImage[bgImage.length - 7]); //gets img number from url
+  let newImgNum = currImgNum + direction; //change img source to next or previous
   const maxImgNum = 6;
 
   if (newImgNum < 1) {
@@ -26,12 +33,11 @@ function changeItem(idName, direction) {
     newImgNum = 1;
   }
 
-  const url = `${idName}` +'/'+`${idName}`+ newImgNum + ".png";//construct new source for img 
+  const url = `${idName}` + "/" + `${idName}` + newImgNum + ".png"; //construct new source for img
   element.setAttribute("style", `background-image:url(${url})`);
 }
 
-
-//function for randomize button 
+//function for randomize button
 function randomize() {
   const elList = ["fronthair", "face", "body", "shoes"];
 
@@ -41,7 +47,7 @@ function randomize() {
     let url = `${elList[i]}/${elList[i]}` + randNum + ".png";
     element.setAttribute("style", `background-image:url(${url})`);
 
-    //to make sure backhair matches fronthair 
+    //to make sure backhair matches fronthair
     if (elList[i] == "fronthair") {
       let element = document.getElementById("backhair");
       let url = "backhair/backhair" + randNum + ".png";
@@ -50,13 +56,12 @@ function randomize() {
   }
 }
 
-//function for choosing doll options using their images 
+//function for choosing doll options using their images
 function chooseItem(idName, imgNum) {
   const element = document.getElementById(`${idName}`);
   const url = `${idName}/${idName}` + imgNum + ".png";
   element.setAttribute("style", `background-image:url(${url})`);
 }
-
 
 function downloadImg() {
   //hide unwated elements of canvas
@@ -82,41 +87,38 @@ function downloadImg() {
 btnRandom.addEventListener("click", randomize);
 btnDownload.addEventListener("click", downloadImg);
 
-
-
-for(let i=0; i<bodyBtns.length;i++){
-  hairBtns[i].addEventListener("click", ()=>{
-    chooseItem("fronthair",i+1);
-    chooseItem("backhair",i+1);
+for (let i = 0; i < bodyBtns.length; i++) {
+  hairBtns[i].addEventListener("click", () => {
+    chooseItem("fronthair", i + 1);
+    chooseItem("backhair", i + 1);
   });
-  faceBtns[i].addEventListener("click", ()=>{
-    chooseItem("face",i+1);
+  faceBtns[i].addEventListener("click", () => {
+    chooseItem("face", i + 1);
   });
-  bodyBtns[i].addEventListener("click", ()=>{
-    chooseItem("body",i+1);
+  bodyBtns[i].addEventListener("click", () => {
+    chooseItem("body", i + 1);
   });
-  shoesBtns[i].addEventListener("click", ()=>{
-    chooseItem("shoes",i+1);
+  shoesBtns[i].addEventListener("click", () => {
+    chooseItem("shoes", i + 1);
   });
 }
 
+for (let i = 0; i < controlBtns.length; i++) {
+  if (i % 2 === 0) {
+    dir = 1;
+  } else {
+    dir = -1;
+  }
+  if (i < 2) {
+    controlBtns[i].addEventListener("click", () => {
+      changeItem(dollParts[i], dir);
+      changeItem(dollParts[dollParts.length - 1], dir);
+    });
+  } else {
+    controlBtns[i].addEventListener("click", () => {
+      changeItem(dollParts[i], dir);
+    });
+  }
+}
 
-
-for (let i =0; i<controlBtns.length;i++){
-    if (i %2 === 0){
-      dir = 1;
-    }else{
-      dir = -1;
-    };
-    if (i<2){
-      controlBtns[i].addEventListener("click",()=>{
-        changeItem(dollParts[i], dir);
-        changeItem(dollParts[dollParts.length-1], dir);
-      });
-  }else{
-      controlBtns[i].addEventListener("click",()=>{
-        changeItem(dollParts[i], dir);
-      });
-  };
-    }
 
